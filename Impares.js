b93c671e02623169f01c2d6a74001a467a398dd0
Jose-Tellez1 <1232100587@univermilenium.edu.mx>
2024-07-08 22:11:34 -0600

    Add files via upload

---
+++ Impares.js
@@ -0,0 +1,6 @@
+let impares = 0;
+
+
+for(let num = 1; num <= 1000; num += 2){
+    impares += num;
+}
---
+++ Pares.js
@@ -0,0 +1,5 @@
+let pares = 0;
+
+for (let numero = 0; numero <= 1000; numero += 2){
+    pares += numero; 
+}
